d2484d5ec1b3e893b5a987b99bf8dc00542369ab
atul <dhitalatul4@gmail.com>
2024-02-10 21:15:35 +0545

    push push

--- c-Programming/class/Functions.c
+++ c-Programming/class/Functions.c
@@ -12,18 +12,44 @@
 // }
 
 //functions having parameterand having value
-int main(){
-    int a,b,sum;
-    printf("enter two numbers");
-    scanf("%d",&a);
-    printf("another number");
+// int main(){
+//     int a,b,sum;
+//     printf("enter two numbers");
+//     scanf("%d",&a);
+//     printf("another number");
     
-    scanf("%d",&b);
-    sum=add(a,b);
-    printf("sum is :%d",sum);
+//     scanf("%d",&b);
+//     sum=add(a,b);
+//     printf("sum is :%d",sum);
+//     return 0;
+// }
+// int add(int a, int b){
+//     return a+b;
+// }
+
+// factrioal  using function 
+#include <stdio.h>
+
+int factorial(int n);
+
+int main()
+{
+    int num, result;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    result = factorial(num);
+    printf("%d\n", result);
     return 0;
 }
-int add(int a, int b){
-    return a+b;
-}
 
+int factorial(int n)
+{
+    int fact = 1;
+    while (n != 0)
+    {
+        fact *= n;
+        n--;
+    }
+
+    return fact;
+}
